0e32170522bca61f20cffb231e600fc6f26c26d7
dattito <32016658+dattito@users.noreply.github.com>
2025-07-15 21:58:57 +0200

    feat: adding causal_masking and batching

--- transformers_from_scratch/bank_dataset.py
+++ transformers_from_scratch/bank_dataset.py
@@ -10,22 +10,23 @@ class BankDataset(Dataset):
         words = " ".join(self.df["Satz"])
         self.c = nltk.Cistem(True)
 
-        self.index2word = ["<unk>"]
+        self.index2word = ["<unk>"]  # Keep as list, don't convert to set
         self.context_window_size = 0
+
+        all_words = set()  # Use a separate set for uniqueness
         for w in nltk.word_tokenize(words, language="german"):
-            self.index2word.append(self.c.stem(w))
+            stemmed = self.c.stem(w)
+            all_words.add(stemmed)
             if len(w) > self.context_window_size:
                 self.context_window_size = len(w)
 
-        self.index2word = set(self.index2word)
+        self.index2word.extend(sorted(all_words))  # Add sorted unique words
         self.unique_word_count = len(self.index2word)
 
-        self.word2index = {}
+        # Create word2index mapping from the ordered list
+        self.word2index = {word: idx for idx, word in enumerate(self.index2word)}
 
         self.label2index = {"Flussbank": 0, "Geldbank": 1, "Sitzbank": 2}
-
-        for i, e in enumerate(self.index2word):
-            self.word2index[e] = i
 
     def __len__(self):
         return len(self.df)
@@ -47,8 +48,12 @@ class BankDataset(Dataset):
 
     def sentence_to_indexes(self, sentence: str):
         nltk_sentence = nltk.word_tokenize(sentence, language="german")
-        indexes = [0 for _ in range(self.context_window_size - len(nltk_sentence))]
+        indexes = []
         for w in nltk_sentence:
             indexes.append(self.word2index[self.c.stem(w)])
+
+        # Right-pad instead of left-pad
+        while len(indexes) < self.context_window_size:
+            indexes.append(0)  # Pad at the end
 
         return indexes
